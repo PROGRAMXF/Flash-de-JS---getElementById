7693d25c8c54ef02b1fa4fb6d813f8476b055443
Fernando <fh1273509@gmail.com>
2022-10-08 19:35:18 -0300

    proyecto listo

---
+++ codigo.js
@@ -0,0 +1,7 @@
+function funcion(){
+
+    //el metodo getElementById hace referencia al elemento id de la imagen colocada en el html//
+    var element = document.getElementById('imagen');
+    element.src = "maquina2.jpg";
+    
+}
